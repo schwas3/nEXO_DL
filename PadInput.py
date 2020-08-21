f07242beeae8af2a1fc12038c3d27a5b37f74aa3
Zepeng Li <zepengl@gmail.com>
2020-08-20 22:45:30 -0400

    add argument parser

--- PadInput.py
+++ PadInput.py
@@ -1,46 +1,63 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import uproot as up
 import numpy as np
+import argparse
+import os
 
-input_TFile = up.open('bb0n_seed1.root') #../Baseline2019_bb0n_FullLXe_seed100.nEXOevents.root')
-elecEvent_TTree = input_TFile['Event/Elec/ElecEvent']
-# We need to ignore the fBits column, which causes errors (and isn't useful anyway.)
-# See the issue for more info: https://github.com/scikit-hep/uproot/issues/475
-#elec_columns_to_get = [x for x in events.allkeys() \
-#                  if events[x].interpretation is not None and x != b"fBits"]
-elec_columns_to_get = [ 'fElecChannels.fChannelCharge',\
+def SaveNumpy(rootfile, npdir):
+    print(rootfile, npdir)
+    if not os.path.exists(npdir):
+        os.sys('mkdir %s' % npdir)
+    input_TFile = up.open(rootfile)
+    elecEvent_TTree = input_TFile['Event/Elec/ElecEvent']
+    elec_columns_to_get = [ 'fElecChannels.fChannelCharge',\
                         'fElecChannels.fWFAmplitude',\
                         'fElecChannels.fNoiseWF',\
                         'fElecChannels.fXPosition',\
                         'fElecChannels.fYPosition',\
                         'fElecChannels.fChannelNoiseTag']
-dfelec = elecEvent_TTree.arrays( elec_columns_to_get, outputtype=pd.DataFrame )
+    dfelec = elecEvent_TTree.arrays( elec_columns_to_get, outputtype=pd.DataFrame )
 
-for i in range(len(dfelec)):
-    row = dfelec.iloc[i]
-    xpos = []
-    ypos = []
-    indices = []
-    npimg = np.zeros((8, 224, 224), dtype=np.float16)
-    for j in range(len(row['fElecChannels.fChannelCharge'])):
-        if row['fElecChannels.fChannelCharge'][j] and row['fElecChannels.fChannelNoiseTag'][j] ==0:
-            xpos.append(row['fElecChannels.fXPosition'][j])
-            ypos.append(row['fElecChannels.fYPosition'][j])
-            indices.append(j)
-    if max(xpos) - min(xpos) > 15*12 or max(ypos) - min(ypos) > 15*12:
-        print('skip large size event')
-        break
-    xmin = min(xpos)
-    ymin = min(ypos)
-    for x, y, index in zip(xpos, ypos, indices):
-        H = int((x - xmin)/12 + (y - ymin)/12*15)
-        for ch in range(8):
-            for W in range(224):
-                samplet = W + 224*ch
-                if samplet >= len(row['fElecChannels.fWFAmplitude'][index]):
-                    continue
-                else:
-                    npimg[ch, H, W] = row['fElecChannels.fWFAmplitude'][index][samplet]
-    np.save('bb0n_%d.npy' % i, npimg)
+    for i in range(len(dfelec)):
+        row = dfelec.iloc[i]
+        xpos = []
+        ypos = []
+        indices = []
+        npimg = np.zeros((8, 224, 224), dtype=np.float16)
+        for j in range(len(row['fElecChannels.fChannelCharge'])):
+            if row['fElecChannels.fChannelCharge'][j] and row['fElecChannels.fChannelNoiseTag'][j] ==0:
+                xpos.append(row['fElecChannels.fXPosition'][j])
+                ypos.append(row['fElecChannels.fYPosition'][j])
+                indices.append(j)
+        xmax = 0
+        xmin = 0
+        ymax = 0
+        ymin = 0
+        if len(xpos) > 0:
+            xmax = max(xpos)
+            xmin = min(xpos)
+        if len(ypos) > 0:
+            ymax = max(ypos)
+            ymin = min(ypos)
 
+        if xmax - xmin > 15*12 or ymax - ymin > 15*12:
+            print('skip large size event')
+            continue
+        for x, y, index in zip(xpos, ypos, indices):
+            H = int((x - xmin)/12 + (y - ymin)/12*15)
+            for ch in range(8):
+                for W in range(224):
+                    samplet = W + 224*ch
+                    if samplet >= len(row['fElecChannels.fWFAmplitude'][index]):
+                        continue
+                    else:
+                        npimg[ch, H, W] = row['fElecChannels.fWFAmplitude'][index][samplet]
+        np.save('%s/%s_%d.npy' % (npdir, os.path.basename(rootfile), i), npimg)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Image Building')
+    parser.add_argument('--eventtype', default='bb0n', type=str, help='Event Type')
+    parser.add_argument('--num', default=1, type=int, help='File Number')
+    args = parser.parse_args()
+    SaveNumpy('/home/zl423/scratch60/pad/%s%d.root' % (args.eventtype, args.num), '/home/zl423/scratch60/pad/%s%d' % (args.eventtype, args.num))
