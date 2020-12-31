6e73273caf88d08ed2728716de1e1d38f9d2d545
Zepeng Li <zepengl@gmail.com>
2020-12-31 13:13:30 +0800

    add hdf5 dataset scripts.

---
+++ h5merger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+'''Reads NeXus HDF5 files using h5py and prints the contents'''
+
+import h5py    # HDF5 support
+import glob
+import time
+import csv
+filelist = glob.glob('/scratchfs/exo/zepengli94/outfiles/*.h5')
+t1 = time.time()
+csvfile = open('dataset_info.csv', 'w')
+fieldnames = ['groupname', 'dsetname']
+writer = csv.DictWriter(csvfile, fieldnames)
+with h5py.File('test1.h5', 'w') as fid:
+    junodata = fid.create_group(u'nexo_data' )
+    for i in range(len(filelist)):
+        fileName = filelist[i]
+        print(fileName, time.time() - t1)
+        f = h5py.File(fileName,  "r")
+        f.copy(f['nexo_data'], junodata, name='nexo_data_%d' % i)
+        dset = f['nexo_data']
+        for item in dset.keys():
+            writer.writerow({'groupname':'nexo_data_%d' % i, 'dsetname':item})
+        f.close()
